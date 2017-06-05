b0c2d1c92b1402da07a6d796d353312cff437f57
kimschmidt <ks178@hdm-stuttgart.de>
2017-06-05 12:05:55 +0200

    Platzhalter

--- upload/test.php
+++ upload/test.php
@@ -1,7 +1,4 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: kimschmidt
- * Date: 05.06.17
- * Time: 11:56
- */
+Dieses File dient als Platzhalter, da
+GitHup sonst die Ordnerstruktur, welche
+für den Mars-Bilder-Upload benötigt wird,
+nicht abbiliden kann.
