519d6659b8bf64241655adec58178baca4cea79c
kimschmidt <ks178@hdm-stuttgart.de>
2017-07-08 10:07:48 +0200

    Anpassung CSS Erweiterung Profilverwaltung

--- Profilverwaltung.php
+++ Profilverwaltung.php
@@ -1,6 +1,5 @@
-
 <!DOCTYPE html>
-<html lang="de">
+<html lang="de" xmlns="http://www.w3.org/1999/html">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -18,32 +17,32 @@
     <link href="Footer.css" rel="stylesheet">
 </head>
 
+
+
 <?php
-include "Backend/session.php"
+include "Backend/session.php";
+include ('Backend/userdata.php');
+$pdo = new PDO($dsn, $dbuser, $dbpass);   //Datenbankzugriff wird erzeugt
+$IDSESSION = $_SESSION ["user_id"];
+
+
+$statement = $pdo->prepare("SELECT * FROM USER WHERE ID = :IDSESSION");
+$result = $statement->execute(array('IDSESSION' => $IDSESSION));
+$user = $statement->fetch();
+$vorname = $user['vorname'];  //Vorname des eingeloggten Users wird zur Begrüßung in der Navigationsleiste angezeigt
 ?>
+
+
+
 
 <body>
 
-<div class="row">
-    <div class="col-md-1"></div>
-    <div class="col-md-1"></div>
-    <div class="col-md-1"></div>
-    <div class="col-md-1"></div>
-    <div class="col-md-1"></div>
-    <div class="col-md-1"></div>
-    <div class="col-md-1"></div>
-    <div class="col-md-1"></div>
-    <div class="col-md-1"></div>
-    <div class="col-md-1"></div>
-    <div class="col-md-1"></div>
-    <div class="col-md-1"></div>
-</div>
 
 <?php
 include "Backend/verwalten.php"
 ?>
 
-<!-- Fixierte Navbar -->
+<!--Navigationsleiste-->
 <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
@@ -53,26 +52,26 @@ include "Backend/verwalten.php"
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Willkommen</a>
+            <a class="navbar-brand" href="#">Willkommen &nbsp;<?PHP echo $vorname; ?></a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
                 <li><a href="Profilseite1.php">Profil</a></li>
+                <li><a href="Galerie.php">Galerie</a></li>
+                <li><a href="Profilseite3.php">Tweets meiner Freunde</a></li>
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Einstellungen <span class="caret"></span></a>
                     <ul class="dropdown-menu">
-                        <li><a href="Profilverwaltung.php">Profilverwaltung</a></li>
+                        <li><a href="Profilverwaltung.php">Profilverwaltung &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="glyphicon glyphicon-edit" aria-hidden="true"</span></a></li>
                         <li role="separator" class="divider"></li>
-                        <li class="dropdown-header">Persönliche Informationen</li>
-                        <li><a href="#">Kontaktdaten</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="LogIn.php">Abmelden</a></li>
+                        <li><a href="Backend/logout.php">Abmelden &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="glyphicon glyphicon-log-out" aria-hidden="true"</span></a></li>
                     </ul>
                 </li>
             </ul>
         </div>
     </div>
 </nav>
+
 
 
 <!-- Profilbild laden -->
@@ -96,7 +95,7 @@ while ($zeile = $query->fetchObject()) {
     <form class="form-horizontal" action="?verwalten=1" method="post">
         <legend>Mein Profil verwalten</legend>
 
-        <!--Profilbild-->
+        <!--Profilbild anzeigen-->
         <div class="container">
             <div class="row">
                 <div class="profile-header-container">
@@ -109,11 +108,24 @@ while ($zeile = $query->fetchObject()) {
         </div>
 
 
-        <br><br>
+        <div class="abstandhalter"></div>
+
+
+
+        <!--Button Profilbild wechseln-->
+        <button type="button" class="btn btn-success-registrieren2" data-toggle="modal" data-target="#Profilbildwechseln">Profilbild ändern</button>
+
+
+
+
+
+<br><br>
+
+
+
 
         <!--Formular Daten ändern-->
-        <div class="container">
-
+<div class="row">
         <div class="form-group">
             <label class="col-md-4 control-label">E-Mail Adresse ändern</label>
             <div class="col-md-4">
@@ -124,27 +136,64 @@ while ($zeile = $query->fetchObject()) {
         </div>
 
 
+
+</div>
+
+        <div class="abstandhalter"></div>
+        <div class="abstandhalter"></div>
+
+
+
+
+        <div class="row">
+
         <div class="form-group">
             <label class="col-md-4 control-label">Passwort ändern</label>
             <div class="col-md-4">
                 <input  name="passwort" type="password" placeholder="aktuelles Passwort" class="form-control input-md">
                 <input  name="passwort1" type="password" placeholder="neues Passwort" class="form-control input-md">
                 <input  name="passwort2" type="password" placeholder="neues Passwort bestätigen" class="form-control input-md">
+                <hr>
+                <button type="submit" class="btn btn-success-registrieren2">Speichern</button><br><br><br>
+                <a href="#"data-toggle="modal" data-target="#Accountdelete">Account löschen </a>
+
+                <!-- Modal -->
+                <div id="Accountdelete" class="modal fade" role="dialog">
+                    <div class="modal-dialog">
+
+                        <!-- Modal content-->
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                <h4 class="modal-title">Account löschen</h4>
+                            </div>
+                            <div class="modal-body">
+                                <p>Willst du wirklich deinen Account löschen?</p>
+                            </div>
+                            <div class="modal-footer">
+                                <a href="Backend/user_delete.php"> <button type="button" class="btn btn-success-registrieren2"  data-dismiss="modal">Account löschen</button></a>
+                                <button type="button" class="btn btn-success-registrieren2" data-dismiss="modal">Abbrechen</button>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
             </div>
         </div>
-            <button type="submit" class="btn btn-success-registrieren2">Speichern</button><br><br>
-    </form>
+        </div>
+</div>
 
+
+</form>
 
 </div>
 
 
-<!--Button Account löschen-->
-<form action="/~ks178/Backend/user_delete.php">
-    <br><br><input type="submit" class="btn btn-success-löschen" value="Account löschen">
-</form>
 
 <br><br><br>
+
+
+
 
 
 
@@ -157,13 +206,35 @@ include "Backend/session.php"
 
 
 
-<div class="formular2">
-    <form enctype="multipart/form-data" action="Backend/profilbild_wechseln.php" method="post">
-        <strong>Profilbild ändern:</strong><br><br>
-        <input type="file" name="datei" /> <br>
-        <input type="submit" value="Tweeten" class="btn-success"/>
-    </form>
+
+<div id="Profilbildwechseln" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+
+
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Lade dein Profilbild hoch</h4>
+            </div>
+            <div class="modal-body">
+                <form enctype="multipart/form-data" action="Backend/profilbild_wechseln.php" method="post">
+                    <strong>Profilbild ändern:</strong><br><br>
+                    <input type="file" name="datei" /> <br>
+                    <input type="submit" value="Bild hochladen" class="btn btn-success-registrieren2"/>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-success-registrieren2" data-dismiss="modal">Abbrechen</button>
+            </div>
+        </div>
+
+    </div>
 </div>
+
+
+
+
+
 
 
 <br><br><br><br><br>
